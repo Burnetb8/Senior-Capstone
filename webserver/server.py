5b82c1de01912af188ecd1de564fb16f1a27926c
meowsome <tycarr01@gmail.com>
2022-09-22 15:56:05 -0400

    Add leaflet folium interactive map to webserver

--- webserver/server.py
+++ webserver/server.py
@@ -1,30 +1,27 @@
 from dash import Dash, html, dcc
 import plotly.express as px
+import folium
+from folium.plugins import MarkerCluster
 import pandas as pd
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+coords = [29.1802, -81.0598]
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+location_map = folium.Map(location=coords, zoom_start=13)
+location_map.save("map.html")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='ATC Map'),
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+    html.Button(id='switch', children='Switch Map'),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    # dcc.Graph(
+    #     id='example-graph',
+    #     figure=fig
+    # )
+
+    html.Iframe(srcDoc=open("map.html","r").read(), width="100%", height="500")
 ])
 
 if __name__ == '__main__':
