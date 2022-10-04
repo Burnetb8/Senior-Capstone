6cb673daf7d3edc9b8e9b743957c36e74bec6e1c
Aaron Van De Brook <avandebrook5@gmail.com>
2022-10-04 12:32:33 -0400

    Add basic example files, boilerplates for manifest utils.

---
+++ examples/train_scratch.py
@@ -0,0 +1,31 @@
+import nemo.collections.asr as nemo_asr
+import pytorch_lightning as pl
+import torch
+import gc
+from ruamel.yaml import YAML
+from omegaconf import DictConfig
+
+config_path = 'config/config.yaml'
+train_manifest = 'manifest/train.json'
+valid_manifest = 'manifest/valid.json'
+checkpoint_name = '100epoch_quartznet_scratch.nemo'
+
+torch.cuda.empty_cache()
+gc.collect()
+
+with open(config_path, 'r') as f:
+    config = YAML(typ='safe').load(f)
+
+config['model']['train_ds']['manifest_filepath'] = train_manifest
+config['model']['train_ds']['batch_size'] = 8
+config['model']['validation_ds']['manifest_filepath'] = valid_manifest
+config['model']['validation_ds']['batch_size'] = 8
+
+model = nemo_asr.models.EncDecCTCModel(cfg=DictConfig(config['model']))
+model.setup_training_data(DictConfig(config['model']['train_ds']))
+model.setup_validation_data(DictConfig(config['model']['validation_ds']))
+
+trainer = pl.Trainer(gpus=[0], max_epochs=100)
+trainer.fit(model)
+
+model.save_to(checkpoint_name)
---
+++ examples/utils/atcc.py
@@ -0,0 +1,7 @@
+"""
+This file contains the necessary functions to preprocess the audio and
+transcript data of the Air Traffic Control Complete dataset and transform
+it into the NeMo manifest format.
+"""
+import os
+
