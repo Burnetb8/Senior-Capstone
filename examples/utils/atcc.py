e61c240cb3bab3fda8eb1f8b2caf184ade8dd1f5
Jakob K. Haehre <haehrej@llb349-01.dbwks.erau.edu>
2023-02-09 15:36:37 -0500

    atcc.py fix

--- examples/utils/atcc.py
+++ examples/utils/atcc.py
@@ -10,10 +10,11 @@ import json
 from lisp_parser import parse
 from typing import Union, List, Dict, Tuple
 from pathlib import Path
+from pydub import AudioSegment
 
 # If I could make this a one-liner I would, but I'm not sure how or if it is possible
-ffmpeg_present = subprocess.run(["ffmpeg" ,"-version"])
-ffmpeg_present = not bool(ffmpeg_present.returncode)
+# ffmpeg_present = subprocess.run(["command", "-v", "ffmpeg"])
+# ffmpeg_present = not bool(ffmpeg_present.returncode)
 
 
 def enumerate_files(
@@ -22,11 +23,9 @@ def enumerate_files(
     """
     Enumerates the audio and corresponding transcript files in the dataset and performs the file conversion from the NIST Sphere
     format (`sph`)to WAV format (`wav`). **Note**: `ffmpeg` must be installed for the file conversion to work.
-
     Arguments:
     ----------
     `root_path`: string or pathlib.Path object, path to the root of the dataset e.g. /data/atc0_ldc94s14a
-
     Returns:
     --------
     `(audio_paths, transcript_paths)`: Tuple of pathlib.Path objects or strings, depending on input type, that contains the audio and
@@ -43,15 +42,19 @@ def enumerate_files(
     # convert to pathlib object to make system path ops easier
     root = Path(root_path).absolute()
 
-    if ffmpeg_present:
-        # check if sphere files need to be converted
-        sphere_paths = glob.glob(str(root.joinpath("**/audio/*.sph")), recursive=True)
-        if len(sphere_paths) > 0:
-            convert_sph_to_wav(sphere_paths)
+    # if ffmpeg_present:
+    # check if sphere files need to be converted
+    sphere_paths = glob.glob(str(root.joinpath("**/audio/*.sph")), recursive=True)
+    if len(sphere_paths) > 0:
+        convert_sph_to_wav(sphere_paths)
 
     # audio and transcript globs
-    audio = glob.glob(str(root.joinpath("**/audio/*.wav")), recursive=True)
-    transcripts = glob.glob(str(root.joinpath("**/transcripts/*.txt")), recursive=True)
+    audio = sorted(glob.glob(str(root.joinpath("**/audio/*.wav")), recursive=True))
+    transcripts = sorted(
+        glob.glob(str(root.joinpath("**/transcripts/*.txt")), recursive=True)
+    )
+
+    assert len(audio) == len(transcripts)
 
     # return tuple of types to match input type i.e. return Path objects for an input of a Path object
     # and strings for an input of a string
@@ -63,18 +66,18 @@ def enumerate_files(
         return [str(x) for x in audio], [str(x) for x in transcripts]
 
 
-def build_atcc_manifests(audio_paths: Union[List[Path], List[str]], transcript_paths: Union[List[Path], List[str]]) -> List[str]:
+def build_atcc_manifests(
+    audio_paths: Union[List[Path], List[str]],
+    transcript_paths: Union[List[Path], List[str]],
+) -> List[str]:
     """
     Parses transcripts and converts it into NeMo manifest format.
-
     Arguments:
     ----------
     `audio_paths`: Either a list of pathlib.Path object or a list of strings containing the paths to the audio files
     (see ~`enumerate_files`).
-
     `transcript_paths`: Either a list of pathlib.Path object or a list of strings containing the paths to the transcript files
     (see ~`enumerate_files`).
-
     Returns:
     -------
     `manifest_data`: A list of JSON formatted strings with the info necessary for NeMo to process and use the data.
@@ -83,21 +86,28 @@ def build_atcc_manifests(audio_paths: Union[List[Path], List[str]], transcript_p
 
     for audio, transcript in zip(audio_paths, transcript_paths):
         audio = Path(audio)
-        with open(transcript, "r") as f:
+
+        with open(transcript, "r", encoding="utf-8") as f:
             transcript_info = parse(f.readlines())
 
         for item in transcript_info:
             try:
-                manifest_lines.append(
-                    json.dumps(
-                        {
-                            "audio_filepath": str(audio.absolute()),
-                            "text": item["TEXT"],
-                            "offset": item["TIMES"]["start"],
-                            "duration": round(item["TIMES"]["end"]- item["TIMES"]["start"], 3),
-                        }
+                duration = round(item["TIMES"]["end"] - item["TIMES"]["start"], 3)
+
+                if duration <= float(1 / 16000):
+                    print(str(audio))
+
+                if item["TEXT"] != "":
+                    manifest_lines.append(
+                        json.dumps(
+                            {
+                                "audio_filepath": str(audio.absolute()),
+                                "text": item["TEXT"].lower(),
+                                "offset": item["TIMES"]["start"],
+                                "duration": duration,
+                            }
+                        )
                     )
-                )
             except KeyError:
                 continue
 
@@ -107,11 +117,9 @@ def build_atcc_manifests(audio_paths: Union[List[Path], List[str]], transcript_p
 def convert_sph_to_wav(paths: Union[List[Path], List[str]]):
     """
     Converts NIST Sphere formatted files to WAV format.
-
     Arguments:
     ----------
     `paths`: paths to the Sphere files to reformat.
-
     Returns:
     --------
     `None`
@@ -119,8 +127,15 @@ def convert_sph_to_wav(paths: Union[List[Path], List[str]]):
     for file in paths:
         if not isinstance(file, Path):
             file = Path(file).absolute()
-        ffmpeg_command = ["ffmpeg", "-y", "-i", str(file), str(file).replace("sph", "wav")]
-        subprocess.run(ffmpeg_command)
+        subprocess_cmd = [
+            "sox",
+            str(file),
+            # "-r",
+            # "16000",
+            str(file).replace(".sph", ".wav"),
+        ]
+        # print(subprocess_cmd)
+        subprocess.run(subprocess_cmd)
 
 
 if __name__ == "__main__":
@@ -132,5 +147,5 @@ if __name__ == "__main__":
 
     # store manifest json in 'manifests' directory
     os.makedirs("manifests", exist_ok=True)
-    with open("manifests/atcc_all.json", 'w') as manifest:
+    with open("manifests/atcc_all.json", "w", encoding="utf-8") as manifest:
         manifest.write("\n".join(manifest_all))
