814b8a5a7cda5ef615ee72ccddace2932da44198
root <root@DESKTOP-FUIRAQM.localdomain>
2023-02-14 13:04:33 -0500

    Citrinet trainer with tokenizer stuff

--- examples/train_citrinet_with_tokenizer.py
+++ examples/train_citrinet_with_tokenizer.py
@@ -10,7 +10,7 @@ train_manifest = 'utils/manifests/atcc_train.json'
 validation_manifest = 'utils/manifests/atcc_validation.json'
 #this is a model found by running test.py looking for EncDecoCTCModelBPE
 base_model_name = 'stt_en_citrinet_512'
-save_as = 'ft100epoch_stt_en_citrinet_512.nemo'
+save_as = 'ft100epoch_stt_en_citrinet_512_tokenizer.nemo'
 
 with open(config_path, 'r') as f:
     config = YAML(typ='safe').load(f)
@@ -25,15 +25,22 @@ config['model']['validation_ds']['manifest_filepath'] = validation_manifest
 
 config['model']['optim']['lr'] = 0.001
 
-base_model = nemo_asr.models.EncDecCTCModelBPE(cfg = DictConfig(config))
+config['model']['tokenizer']['dir'] = "test_tokenizer.model"
 
-base_model.change_vocabulary(new_tokenizer_dir = DictConfig(config['tokenizer']['dir']), new_tokenizer_type = DictConfig(config['tokenizer']['type']))
+config['model']['tokenizer']['type'] = "bpe"
+
+trainer = pl.Trainer(gpus=[0], max_epochs=100)
+
+base_model = nemo_asr.models.EncDecCTCModelBPE(cfg = DictConfig(config['model']))
+#base_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(base_model_name)
+
+base_model.change_vocabulary(new_tokenizer_dir = DictConfig(config['model']['tokenizer']['dir']), new_tokenizer_type = DictConfig(config['model']['tokenizer']['type']))
 
 base_model.setup_optimization(optim_config=DictConfig(config['model']['optim']))
 base_model.setup_training_data(train_data_config=DictConfig(config['model']['train_ds']))
 base_model.setup_validation_data(val_data_config=DictConfig(config['model']['validation_ds']))
 
-trainer = pl.Trainer(gpus=[0], max_epochs=100)
+
 trainer.fit(base_model)
 
 base_model.save_to(save_as)
