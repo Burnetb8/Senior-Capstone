4cd78e7fdebed823f0ba7ef746c61cce4eeac8a4
Jakob K. Haehre <haehrej@llb349-01.dbwks.erau.edu>
2023-02-16 15:40:41 -0500

    trans given audio is an object to trans and test trans shows how to use it

---
+++ examples/transcribe_given_audio_file.py
@@ -0,0 +1,27 @@
+import nemo.collections.asr as nemo_asr
+import pytorch_lightning as pl
+import torch
+import gc
+from ruamel.yaml import YAML
+from omegaconf import DictConfig
+from omegaconf import OmegaConf, open_dict
+
+
+
+
+class Transcribe_ATC:
+
+
+    def __init__(self):
+        #EncDecCTCModelBPE is for citrinet models you will need to change this depending
+        #on the model we are using 
+        self.model_check_point = 'ft100epoch_stt_en_citrinet_512_2.nemo'
+        self.base_model = nemo_asr.models.EncDecCTCModelBPE.restore_from(self.model_check_point)
+
+    def transcribe_audio(self,file_name):
+        files = [file_name]
+        self.base_model.transcribe(paths2audio_files=files, batch_size=1) 
+
+
+
+
