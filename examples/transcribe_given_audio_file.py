c4b0509fd1dc034ca435156128c0855acf38baab
Burnetb8 <113138091+Burnetb8@users.noreply.github.com>
2023-02-22 16:56:43 -0500

    Update transcribe_given_audio_file.py

--- examples/transcribe_given_audio_file.py
+++ examples/transcribe_given_audio_file.py
@@ -15,8 +15,12 @@ class Transcribe_ATC:
     def __init__(self):
         #EncDecCTCModelBPE is for citrinet models you will need to change this depending
         #on the model we are using 
+        #EncDecCTCModel is for quartznet and jasper
         self.model_check_point = 'ft100epoch_stt_en_citrinet_512_2.nemo'
+        try:
         self.base_model = nemo_asr.models.EncDecCTCModelBPE.restore_from(self.model_check_point)
+    except:
+        self.base_model = nemo_asr.models.EncDecCTCModel.restore_from(self.model_check_point)
 
     def transcribe_audio(self,file_name):
         files = [file_name]
