2b44417024d94de85751aced787a4d63532009f0
avandebrook <avandebrook5@gmail.com>
2023-05-15 20:53:30 -0400

    Add plane marker and info display.

--- source/blueprints/map.py
+++ source/blueprints/map.py
@@ -1,12 +1,21 @@
-from flask import Blueprint, render_template
 from enum import Enum
+import json
+
+from flask import Blueprint, render_template, make_response
+from opensky_api import OpenSkyApi
+from ..secrets import OpenSkyCredentials
+
 
 bp = Blueprint("map", __name__, url_prefix="/map")
 
-initial_center = {"lon": -81.0598, "lat": 29.1802, "zoom": 8}
+initial_center = {"lon": -81.0580422, "lat": 29.1799089, "zoom": 8}
 #! Initial latitude and longitude set to Daytona Beach. Zoom level found through trial and error
 
-markers = [{"lat": 29.1802, "lon": -81.098}]
+opensky = OpenSkyApi(
+    username=OpenSkyCredentials.USERNAME, password=OpenSkyCredentials.PASSWORD
+)
+
+# markers = [{"lat": 29.1802, "lon": -81.098}]
 #! Latitudinal and longitudinal coordinates of the planes from the opensky API
 
 
@@ -17,9 +26,53 @@ class MapSelector(Enum):
 
 @bp.route("/<type>")
 def map(type: str):
-    if type == "geo":
+    map_type = MapSelector(type)
+    if map_type == MapSelector.GEOGRAPHIC:
         return render_template(
-            "map/geographic_map.html", markers=markers, initial_center=initial_center
+            "map/geographic_map.html",
+            initial_center=initial_center,
         )
-    elif type == "aero":
-        return render_template("map/sectional_map.html", markers=markers)
+    elif map_type == MapSelector.SECTIONAL:
+        return render_template(
+            "map/sectional_map.html",
+            initial_center=initial_center,
+        )
+
+
+@bp.route("plane_states")
+def plane_states():
+    data = {"plane_data": []}
+    states = opensky.get_states(
+        bbox=(
+            initial_center["lat"] - 3,
+            initial_center["lat"] + 3,
+            initial_center["lon"] - 3,
+            initial_center["lon"] + 3,
+        )
+    ).states
+
+    for state in states:
+        data["plane_data"].append(
+            {
+                "icao24": state.icao24,
+                "callsign": state.callsign,
+                "origin_country": state.origin_country,
+                "time_position": state.time_position,
+                "last_contact": state.last_contact,
+                "longitude": state.longitude,
+                "latitude": state.latitude,
+                "geo_altitude": state.geo_altitude,
+                "on_ground": state.on_ground,
+                "velocity": state.velocity,
+                "true_track": state.true_track,
+                "vertical_rate": state.vertical_rate,
+                "squawk": state.squawk,
+                "position_source": state.position_source,
+                "category": state.category,
+            }
+        )
+
+    return make_response(
+        data,
+        200,
+    )
--- source/static/map.js
+++ source/static/map.js
@@ -1,5 +1,112 @@
 var map = L.map('map').fitWorld();
+var planeLayer = L.layerGroup().addTo(map);
+var infoLayer = L.layerGroup().addTo(map).setZIndex(800);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
 }).addTo(map);
+
+$("#closeButton").on('click', (event) => {
+    $("#infoPane").hide();
+    $(".leaflet-control-zoom").show();
+});
+
+function get_position_source_string(position_source) {
+    var source = "";
+
+    switch (position_source) {
+        case 0:
+            source = "ADS-B";
+            break;
+        case 1:
+            source = "ASTERIX";
+            break;
+        case 2:
+            source = "MLAT";
+            break;
+        case 3:
+            source = "FLARM";
+            break;
+        default:
+            source = "N/A";
+    }
+
+    return source;
+}
+
+function get_plane_category_string(category) {
+    var category = "";
+
+    switch (category) {
+        default:
+            category = "Unknown";
+    }
+
+    return category;
+}
+
+function main() {
+    // create http request object
+    const request = new XMLHttpRequest();
+
+    // add event listener for when a response is received
+    request.addEventListener("load", () => {
+        // ignore cases where response body is null
+        if (request.response == null)
+            return;
+
+        planeLayer.clearLayers();
+
+        console.log(request.response.plane_data)
+        for (const plane of request.response.plane_data) {
+            var marker = L.marker({
+                lat: plane.latitude,
+                lng: plane.longitude,
+            });
+
+            // order in which these methods are called doesn't matter
+            marker.setIcon(planeIcon);
+            // Rotate icon to match actual plane heading
+            marker.setRotationAngle(plane.true_track);
+            marker.addTo(planeLayer);
+
+            marker.on('click', (ev) => {
+                // Remove previous data
+                $("#infoPane tr").each(function (index) {
+                    if ($(this).attr("id") != "tableHeader") {
+                        $(this).remove();
+                    }
+                });
+
+                // Add plane entries entries
+                $("#infoPane table").append(`<tr><td>ICAO 24-bit Address</td><td>${plane.icao24.toUpperCase()}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Callsign</td><td>${plane.callsign}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Country Origin</td><td>${plane.origin_country}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Time of Last Position Report</td><td>${Date(plane.time_position)}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Last Contact (time)</td><td>${Date(plane.last_contact)}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Position (Latitude, Longitude)</td><td>${plane.latitude}\u00b0N ${plane.longitude}\u00b0W</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Geometric Altitude (m)</td><td>${plane.geo_altitude}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>On Ground?</td><td>${plane.on_ground ? "Yes" : "No"}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Velocity (m/s)</td><td>${plane.velocity}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Heading (True Track, in degrees)</td><td>${plane.true_track}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Vertical Rate (m/s)</td><td>${plane.vertical_rate}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Squawk</td><td>${plane.squawk}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Position Source</td><td>${get_position_source_string(plane.position_source)}</td></tr>`);
+                $("#infoPane table").append(`<tr><td>Plane Category</td><td>${get_plane_category_string(plane.category)}</td></tr>`);
+
+                // Hide zoom controls (draws over top of the table)
+                $(".leaflet-control-zoom").hide();
+
+                // Display data
+                $("#infoPane").show();
+            });
+        }
+    });
+
+    // Create a GET request to send to the plane_states endpoint
+    request.open("GET", "/map/plane_states");
+    // Specify a JSON return type
+    request.responseType = "json";
+    // Send request
+    request.send();
+}
