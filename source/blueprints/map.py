a6838b94fc12767fe896935e7921b6b50677f7db
TheCreepOfWar <maxwell.m1024@gmail.com>
2023-10-25 13:37:45 -0400

    Initial commit.

--- source/blueprints/map.py
+++ source/blueprints/map.py
@@ -5,22 +5,10 @@ from flask import Blueprint, render_template
 bp = Blueprint("map", __name__, url_prefix="/map")
 
 initial_center = {"lon": -81.0580422, "lat": 29.1799089, "zoom": 8}
+
+
+
 #! Initial latitude and longitude set to Daytona Beach. Zoom level found through trial and error
-
-
-class MapSelector(Enum):
-    """
-    Map type selector.
-
-    Keys correspond to the URI endpoint for the map type.
-    """
-
-    GEOGRAPHIC = "geo"
-    #! Geographic map endpoint selector
-
-    SECTIONAL = "aero"
-    #! Sectional map endpoint selector
-
 
 @bp.route("/<type>")
 def map(type: str):
@@ -31,15 +19,7 @@ def map(type: str):
 
     :param type: Map type to display. Can be either `"geo"` or `"aero"`.
     """
-    map_type = MapSelector(type)
-    if map_type == MapSelector.GEOGRAPHIC:
-        return render_template(
-            "map/geographic_map.html",
-            initial_center=initial_center,
-        )
-
-    elif map_type == MapSelector.SECTIONAL:
-        return render_template(
-            "map/sectional_map.html",
-            initial_center=initial_center,
-        )
+    return render_template(
+        "map/geographic_map.html",
+        initial_center=initial_center,
+    )
